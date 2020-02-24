49955714ce5a1eebd90f09e73c7a9a50e7a43296
Alexandre <aleqsd@gmail.com>
2020-02-24 14:11:17 +0100

    Multiple upload

--- App.js
+++ App.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
-import  {Text,View} from 'react-native';
+import React, { Component } from "react";
+import { Text, View } from "react-native";
 
-import RNFileShareIntent from 'react-native-file-share-intent';
- 
+import RNFileShareIntent from "react-native-file-share-intent";
+
 export default class App extends Component {
   constructor(props) {
-    super(props); 
+    super(props);
     this.state = {
-      fileUrl: null,
+      fileUrl: null
     };
   }
- 
+
   componentDidMount() {
-   if(RNFileShareIntent){
-     RNFileShareIntent.getFilepath((url) => {
-       this.setState({ fileUrl: url }); 
-       })  
-   }
-  
+    if (RNFileShareIntent) {
+      RNFileShareIntent.getFilepath(url => {
+        this.setState({ fileUrl: url });
+      });
+    }
   }
   render() {
     var uri = this.state.fileUrl;
     return (
-      <View style={{flex:1,justifyContent:'center'}}>
-        <Text style={{fontSize:30,color:'black'}}>Shared Url: {uri}</Text>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Text style={{ fontSize: 20, color: "black" }}>Shared Url: {uri}</Text>
       </View>
     );
   }
 }
- 
