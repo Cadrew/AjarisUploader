862dd5da55ce23da79a9e77b6a877b2b25093383
Alexandre <aleqsd@gmail.com>
2020-02-25 16:11:50 +0100

    Fused

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+import Navigation from "./Navigation/Navigation";
+import { Provider } from "react-redux";
+import Store from "./Store/configureStore";
 
 import RNFileShareIntent from "react-native-file-share-intent";
 
@@ -21,9 +24,10 @@ export default class App extends Component {
   render() {
     var uri = this.state.fileUrl;
     return (
-      <View style={{ flex: 1, justifyContent: "center" }}>
+      <Provider store={Store}>
         <Text style={{ fontSize: 20, color: "black" }}>Shared Url: {uri}</Text>
-      </View>
+        <Navigation />
+      </Provider>
     );
   }
 }
